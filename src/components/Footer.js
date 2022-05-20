4cd92de6d7e310b06784960024b89905ce3dd2ba
Jalen Moore <100977121+moorejalen2800@users.noreply.github.com>
2022-05-19 23:40:06 -0400

    Update Footer.js
    deleted useless code and fixed minor misspell

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,9 @@
 import React from "react";
 
 const Footer = () => {
-  const footerStyles = {
-    position: "sticky",
-    background: "#52B7B5",
-    width: '100%'
-  }
+  
     return (
-        <footer className="d-flex flex-wrap  align-items-center py-3 my-4 border-top" style={footerStyles}>
+        <footer className="d-flex flex-wrap  align-items-center py-3 my-4 border-top" style={Footer}>
         <div className="col-md-4 d-flex align-items-center">
           <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
             <svg className="bi" width="30" height="24"></svg>
@@ -23,7 +19,7 @@ const Footer = () => {
           <li className="ms-3"><i className="bi bi-linkedin"></i></li>
           </a>
           <a className="text-muted" href="http://www.github.com/moorejalen2800">
-          <li className="ms-3"><i className="bi bi-twitter"></i></li>
+          <li className="ms-3"><i className="bi bi-github"></i></li>
           </a>
         </ul>
       </footer>
